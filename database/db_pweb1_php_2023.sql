34c5b91def9f643647574dfc5da63f84ddef3264
Jackson Meires <lordjackson@gmail.com>
2023-10-31 14:44:37 -0300

    Update db_pweb1_php_2023.sql

--- database/db_pweb1_php_2023.sql
+++ database/db_pweb1_php_2023.sql
@@ -4,15 +4,8 @@
 -- OS do Servidor:               Win64
 -- HeidiSQL Versão:              12.1.0.6537
 -- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE IF NOT EXISTS `db_aula_pweb1` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_aula_pweb1`;
 
 -- Copiando estrutura para tabela db_aula_pweb1.aluno
 CREATE TABLE IF NOT EXISTS `aluno` (
@@ -26,14 +19,5 @@ CREATE TABLE IF NOT EXISTS `aluno` (
 -- Copiando dados para a tabela db_aula_pweb1.aluno: ~6 rows (aproximadamente)
 INSERT INTO `aluno` (`id`, `nome`, `cpf`, `telefone`) VALUES
 	(1, 'Jackson', '1231230', '123123'),
-	(2, 'Jackson', '55500055599', '49 8800-5500'),
-	(3, 'Jackson', '55500055599', '49 8800-5500'),
-	(4, 'Jackson', '55500055599', '49 8800-5500'),
-	(21, 'Jackson Five', '00055500055', '84 988665500'),
-	(22, 'Arthur 123', '123123123', '49 4654645');
+	(2, 'Aluno Teste', '55500055599', '49 8800-5500'),
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
